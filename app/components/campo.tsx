c5f78d6fc08a43f56d722ee552e85a96145262ed
Everton Santos <everton_projeto@outlook.com>
2025-01-21 18:59:05 -0300

    usando medidas dinamicas

--- app/components/campo.tsx
+++ app/components/campo.tsx
@@ -7,18 +7,18 @@ export default function Campo() {
           "repeating-linear-gradient(#2d561c, #2d561c 9.09%, #356029 9.09%, #356029 18.18%)",
       }}
     >
-      <section className="flex justify-center">
-        <div className="flex justify-center h-[8.56rem] w-[20.14rem] border-2 border-t-0 border-white">
-          <div className="h-[3.0rem] w-[10.0rem] border-2 border-t-0 border-white"></div>
+      <section className="flex justify-center flex-1">
+        <div className="flex justify-center h-1/2 w-1/2 border-2 border-t-0 border-white">
+          <div className="h-2/5 w-1/2 border-2 border-t-0 border-white"></div>
         </div>
       </section>
-      <section className="flex justify-center items-center relative">
+      <section className="flex justify-center items-center relative flex-1">
         <div className="bg-white h-0.5 w-full absolute "></div>
         <div className="h-[9.15rem] w-[9.15rem] rounded-full border-2 border-white"></div>
       </section>
-      <section className="flex justify-center ">
-        <div className="flex justify-center h-[8.56rem] w-[20.14rem] border-2 border-b-0 border-white items-end">
-          <div className="h-[3.0rem] w-[10.0rem] border-2 border-b-0 border-white"></div>
+      <section className="flex justify-center flex-1 items-end">
+        <div className="flex justify-center h-1/2 w-1/2 border-2 border-b-0 border-white items-end">
+          <div className="h-2/5 w-1/2 border-2 border-b-0 border-white"></div>
         </div>
       </section>
     </article>
