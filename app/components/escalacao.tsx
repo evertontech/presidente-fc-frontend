7a598850d943988a83d6349da09a1dd628507373
Everton Santos <everton_projeto@outlook.com>
2025-01-24 19:01:08 -0300

    criando componente escalacao

---
+++ app/components/escalacao.tsx
@@ -0,0 +1,33 @@
+import Campo from "./campo";
+import Jogador from "./jogador";
+
+export default function Escalacao() {
+  return (
+    <div className="flex justify-center items-center h-full w-full relative">
+      <Campo className=""></Campo>
+      <article className="h-2/3 max-h-[50rem] w-full max-w-[35rem] absolute grid grid-rows-5 pt-5">
+        <section className="flex flex-1 justify-center">
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+        </section>
+        <section className="flex flex-1 justify-evenly">
+          <Jogador className=""></Jogador>
+        </section>
+        <section className="flex flex-1 justify-center">
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+        </section>
+        <section className="flex flex-1 justify-evenly">
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+          <Jogador className=""></Jogador>
+        </section>
+        <section className="flex flex-1 justify-evenly">
+          <Jogador className=""></Jogador>
+        </section>
+      </article>
+    </div>
+  );
+}
