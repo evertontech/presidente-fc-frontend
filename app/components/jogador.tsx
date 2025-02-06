fd64d56e3ad17668c06e6b9bcb69d6b6cdfc529b
Everton Santos <everton_projeto@outlook.com>
2025-02-06 18:24:16 -0300

    atualizando nome e posicao

--- app/components/jogador.tsx
+++ app/components/jogador.tsx
@@ -45,14 +45,14 @@ export default function Jogador(props: Props) {
   return (
     <article className={`flex flex-col items-center ${props.className}`}>
       <section className="flex">
-        <span className=" bg-black/40 text-[10px] text-nowrap  text-white px-1 rounded-xl w-fit text-center font-bold">
+        <span className="bg-black/40 text-[10px] text-nowrap  text-white px-1 rounded-xl w-fit text-center font-bold align-middle">
           {dados.pontuacao} pontos
         </span>
       </section>
       <img className="h-1/2" src={avatar} alt="Avatar" />
-      <section className="flex -m-1">
-        <span className={`text-[10px] px-1 rounded-xl w-fit text-center font-bold rounded-r-none ${props.posicao.corFonte} ${props.posicao.corFundo}`}>
-          <h1>{props.posicao.abreviacao}</h1>
+      <section className="flex -m-1 align-middle justify-center items-center">
+        <span className={` align-middle text-[10px] px-1 rounded-xl w-fit font-bold rounded-r-none ${props.posicao.corFonte} ${props.posicao.corFundo}`}>
+          {props.posicao.abreviacao}
         </span>
         <span className="text-[10px] bg-[#131921] text-white px-1 rounded-l-none w-fit text-center font-bold rounded-xl">
           {dados.nome}
--- app/enums/posicaoEnum.ts
+++ app/enums/posicaoEnum.ts
@@ -2,61 +2,61 @@ const PosicaoEnum = Object.freeze({
   GOLEIRO: {
     abreviacao: "GO",
     corFonte: "text-[#FF5733]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   ZAGUEIRO: {
     abreviacao: "ZC",
     corFonte: "text-[#FFC300]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   LATERAL_DIREITO: {
     abreviacao: "LD",
     corFonte: "text-[#FFC300]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   LATERAL_ESQUERDO: {
     abreviacao: "LE",
     corFonte: "text-[#FFC300]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   VOLANTE: {
     abreviacao: "VOL",
     corFonte: "text-[#24ac0f]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   MEIA_ATACANTE: {
     abreviacao: "MAT",
     corFonte: "text-[#24ac0f]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   MEIA_DE_LIGAÇÃO: {
     abreviacao: "MLG",
     corFonte: "text-[#24ac0f]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   PONTA_ESQUERDA: {
     abreviacao: "PE",
     corFonte: "text-[#f82d9c]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   PONTA_DIREITA: {
     abreviacao: "PD",
     corFonte: "text-[#f82d9c]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 
   CENTRO_AVANTE: {
     abreviacao: "CA",
     corFonte: "text-[#f82d9c]",
-    corFundo: "bg-[#333333]",
+    corFundo: "bg-[#131921]",
   },
 });
 
