6f428ea1264948b44d833903f556da37e9f4541e
Everton Santos <everton_projeto@outlook.com>
2025-02-12 18:12:09 -0300

    removendo icone barra

--- app/components/barraDeNavegacao.tsx
+++ app/components/barraDeNavegacao.tsx
@@ -1,23 +1,18 @@
-import { Home, Presentation, Trophy, User, UserRoundCog } from "lucide-react";
 import { Button } from "~/components/ui/button";
 
 export default function BarraDeNavegacao() {
   return (
     <nav className="flex justify-around items-center bg-white w-full h-20 fixed bottom-0">
       <Button variant="ghost" className="flex flex-col">
-        <Home />
         Início
       </Button>
       <Button variant="ghost" className="flex flex-col">
-        <UserRoundCog />
         Escalação
       </Button>
       <Button variant="ghost" className="flex flex-col">
-        <Presentation />
         Tática
       </Button>
       <Button variant="ghost" className="flex flex-col">
-        <Trophy />
         Ligas
       </Button>
     </nav>
