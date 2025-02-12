dabbab3e176bac53b72890f51304381164f3e703
Everton Santos <everton_projeto@outlook.com>
2025-02-12 18:03:26 -0300

    removendo barra de navegacao

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Home, Presentation, Trophy, User, UserRoundCog } from "lucide-react";
 import Escalacao from "~/components/escalacao";
-import { Button } from "~/components/ui/button";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,24 +17,6 @@ export default function Index() {
   return (
     <main className="h-screen flex flex-col items-center justify-center bg-gray-100">
       <Escalacao></Escalacao>
-      <nav className="flex justify-around items-center bg-white w-full h-20 fixed bottom-0">
-        <Button variant="ghost" className="flex flex-col">
-          <Home />
-          Início
-        </Button>
-        <Button variant="ghost" className="flex flex-col">
-          <UserRoundCog />
-          Escalação
-        </Button>
-        <Button variant="ghost" className="flex flex-col">
-          <Presentation />
-          Tática
-        </Button>
-        <Button variant="ghost" className="flex flex-col">
-          <Trophy />
-          Ligas
-        </Button>
-      </nav>
     </main>
   );
 }
